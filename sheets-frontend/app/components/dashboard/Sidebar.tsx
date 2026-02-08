d01b032bf765a2fba12e621da65945ebd6c2f498
flame7pro <64305802+flame7pro@users.noreply.github.com>
2026-02-08 17:51:30 +0530

    Update Sidebar.tsx

--- sheets-frontend/app/components/dashboard/Sidebar.tsx
+++ sheets-frontend/app/components/dashboard/Sidebar.tsx
@@ -258,7 +258,7 @@ export const Sidebar: React.FC<SidebarProps> = ({
                 Device Requests &nbsp;
                 {pendingDeviceRequests && pendingDeviceRequests > 0 && (
                   <span className="absolute -top-1 -right-1 w-5 h-5 bg-rose-500 text-white text-xs font-bold rounded-full flex items-center justify-center shadow-lg">
-                    ({pendingDeviceRequests})
+                    {pendingDeviceRequests}
                   </span>
                 )}
               </button>
